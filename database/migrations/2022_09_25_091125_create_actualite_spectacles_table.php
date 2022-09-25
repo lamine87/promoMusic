672e5cf38b59e0ab263d7d61a5bd9b294eb79aff
lamine87 <laminenatidiarra@yahoo.fr>
2022-09-25 11:18:40 +0200

    Adding ActualiteSpectacle

---
+++ database/migrations/2022_09_25_091125_create_actualite_spectacles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActualiteSpectaclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actualite_spectacles', function (Blueprint $table) {
+            $table->unsignedBigInteger('actualite_id');
+            $table->foreign('actualite_id')->references('id')->on('actualites')->onDelete('cascade');
+
+            $table->unsignedBigInteger('spectacle_id');
+            $table->foreign('spectacle_id')->references('id')->on('spectacles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actualite_spectacles');
+    }
+}
